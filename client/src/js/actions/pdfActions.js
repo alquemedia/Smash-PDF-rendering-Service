7013b01dea1afb375bcadf2b321f6e7152c9c840
Alquemedia S.A.S <david@alquemedia.com>
2017-04-19 16:47:17 -0500

    Finalizing demo

--- client/src/js/actions/pdfActions.js
+++ client/src/js/actions/pdfActions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function generatePDF( params ) {
   return {
     type: "GENERATE_PDF",
-    payload: axios.post("http://smashpdf-server.alquemedia.com",{
+    payload: axios.post( "http://smashpdf-server.alquemedia.com",{
       json: JSON.parse(params.json),
       template: params.template
     })
